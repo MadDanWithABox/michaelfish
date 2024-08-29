288190841d395adeac56178c3d53017d908763ad
Daniel Jordan <maddanwithabox@gmail.com>
2024-08-29 18:13:07 +0100

    feat: update routes for london namespace

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from apis.v1.iris import router as iris_ns
 from apis.v1.boston import router as boston_ns
+from apis.v1.london import router as london_ns
 
 # Initialize logging
 logger.add("./logs/katana.log", rotation="500 MB")
-logger.info("Initializing application : Katana")
+logger.info("Initializing application : MichaelFish")
 
 app = FastAPI(
-    title="Katana ML FastAPI Serving ⚡",
+    title="Michael Fish Weather Forecasting⚡",
     version=1.0,
-    description="FastAPI based template for ASAP ML API development 👩‍💻",
+    description="Only the most accurate of weather forecasts 👩‍💻",
 )
 logger.info("Adding Iris namespace route")
 app.include_router(iris_ns)
 logger.info("Adding Boston namespace route")
 app.include_router(boston_ns)
+logger.info("Adding London Weather namespace route")
+app.include_router(london_ns)
 
 
 @app.get("/", include_in_schema=False)
