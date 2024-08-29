50504ce1c27f0435e5f4ad80572d1288419a7441
Daniel Jordan <maddanwithabox@gmail.com>
2024-08-29 17:55:39 +0100

    feat: update base model to include Evaluation Responses

--- apis/models/base.py
+++ apis/models/base.py
@@ -4,3 +4,10 @@ from pydantic import BaseModel
 class TrainingStatusResponse(BaseModel):
     trainingId: str = "056b5d3d-f983-4cd3-8fbd-20b8dad24e0f"
     status: str = "Training  queued"
+
+
+class EvalStatusResponse(BaseModel):
+    eval_id: str
+    mean_absolute_error: float
+    mean_squared_error: float
+    r2_score: float
